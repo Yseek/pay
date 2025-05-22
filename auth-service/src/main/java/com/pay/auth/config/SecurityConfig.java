030e687cee892fd261a2ea969181a0b5b9960d7c
Yseek <yun5003@naver.com>
2025-05-22 04:51:11 +0000

    Reapply "✨ 인증토큰 로그인 테스트 추가"
    This reverts commit 6d3deb8633dd5cef7b9e8026e55f463dface7bb8.

--- auth-service/src/main/java/com/pay/auth/config/SecurityConfig.java
+++ auth-service/src/main/java/com/pay/auth/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.pay.auth.config;
 
+import com.pay.auth.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,12 +21,14 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth.requestMatchers("/signup", "login").permitAll()
                                 .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
