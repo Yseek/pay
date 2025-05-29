3cbde4b732795987eb3867d0c71204df08b663ad
Yseek <yun5003naver.com>
2025-05-29 11:05:41 +0000

    :sparkles: Auth-service ExceptionHandler 추가

---
+++ auth-service/src/main/java/com/pay/auth/exception/AuthExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.pay.auth.exception;
+
+import com.pay.common.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AuthExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<?>> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(ApiResponse.error(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("서버 오류가 발생했습니다."));
+    }
+}
