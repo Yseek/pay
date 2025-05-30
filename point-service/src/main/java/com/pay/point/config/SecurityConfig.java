ca4f3e3ad6773469d839da6ac04c2523a387ff25
Yseek <yun5003@naver.com>
2025-05-30 04:25:33 +0000

    :lock: Security 설정 변경: 모든 요청 JWT 인증 필요하도록 변경

--- point-service/src/main/java/com/pay/point/config/SecurityConfig.java
+++ point-service/src/main/java/com/pay/point/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
